9bb9811dd3bdf98edcb6fe93715a60dad6e984d5
Robert Rosebury <admin@innovationation.co.uk>
2018-04-22 13:58:21 +0100

    List users api: Test the api structure for the user and get the users from the database.

--- app/Http/Controllers/Api/Users/ListUsersAction.php
+++ app/Http/Controllers/Api/Users/ListUsersAction.php
@@ -3,16 +3,22 @@
 namespace App\Http\Controllers\Api\Users;
 
 use App\Http\Controllers\Controller as BaseController;
+use App\User as UserModel;
 
 class ListUsersAction extends BaseController
 {
-    public function __invoke()
+    public function __invoke(UserModel $user)
     {
+        $users = $user->orderBy('name', 'asc')->get();
+
         return response()->json([
             'data' => [
-                'users' => [
-                    // @todo - list user models
-                ]
+                'users' => $users->transform(function ($user) {
+                    return [
+                        'name' => $user->name,
+                        'email' => $user->email,
+                    ];
+                })
             ]
         ]);
     }
--- tests/Feature/Endpoints/Api/ListUsersActionTest.php
+++ tests/Feature/Endpoints/Api/ListUsersActionTest.php
@@ -2,8 +2,9 @@
 
 namespace Tests\Feature\Endpoints\Api;
 
-use Tests\Feature\Endpoints\AbstractEndpointTest;
+use App\User as UserModel;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\Feature\Endpoints\AbstractEndpointTest;
 
 class ListUsersActionTest extends AbstractEndpointTest
 {
@@ -11,9 +12,16 @@ class ListUsersActionTest extends AbstractEndpointTest
 
     public function testSuccessfullyListingOutUsers()
     {
-        // Call api /api/users
-        // Get all the users from the database
-        // List them out.
+        $this->createUser([
+            'name' => 'John Doe',
+            'email' => 'john.doe@example.co.uk',
+        ]);
+        $this->createUser([
+            'name' => 'Jane Doe',
+            'email' => 'jane.doe@example.co.uk',
+        ]);
+
+        // Call api /api/users and check the basic api structure.
         $response = $this->getJson($this->endpoint());
         $response
             ->assertStatus(200)
@@ -23,13 +31,34 @@ class ListUsersActionTest extends AbstractEndpointTest
                         'users'
                     ]
                 ]
-            );
+            )
+            ->assertJsonCount(2, 'data.users');
 
-        dd($response, $response->content());
+
+        $expectedUsers = UserModel::orderBy('name', 'asc')->get();
+        $users = $response->json()['data']['users'];
+
+        // Check the user structure and that data is ordered as expected.
+        foreach ($users as $key => $user) {
+            $response->assertJsonStructure(
+                [
+                    'name',
+                    'email',
+                ],
+                $user
+            );
+            $this->assertEquals($expectedUsers[$key]->name, $user['name']);
+            $this->assertEquals($expectedUsers[$key]->email, $user['email']);
+        }
     }
 
     protected function endpoint(): string
     {
         return '/api/users';
+    }
+
+    private function createUser(array $overrides = [])
+    {
+        factory(UserModel::class)->create($overrides);
     }
 }
